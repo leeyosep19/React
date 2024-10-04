17108fa7d4e9324d2643c2b1ab0967964052b586
leeyosep19 <dytpqdytqp70@naver.com>
2024-10-04 11:36:58 +0900

    Add files via upload

---
+++ react1/chatapp-be-student/Models/chat.js
@@ -0,0 +1,18 @@
+const { type } = require("express/lib/response");
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema(
+    {
+        chat: String,
+        user:{
+        id:{
+            type: mongoose.Schema.ObjectId,
+            ref:"User",
+        },
+        name: String,
+        },
+    },
+    { timestamp: true }
+);
+
+module.exports = mongoose.model("Chat", chatSchema);
